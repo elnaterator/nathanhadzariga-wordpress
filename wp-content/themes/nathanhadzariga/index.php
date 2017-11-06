d61086393aff46a5db6a76a0c3d3c7fe60c19453
EC2 Default User <ec2-user@ip-172-31-20-216.us-west-2.compute.internal>
2017-11-06 07:49:20 +0000

    Lots of stuff

--- wp-content/themes/nathanhadzariga/index.php
+++ wp-content/themes/nathanhadzariga/index.php
@@ -2,15 +2,17 @@
 
 <div class="content">
 <div class="container">
+
   <?php
   query_posts(array('showposts' => -1, 'post_type' => 'page', 'orderby' => 'menu_order'));
   if( have_posts() ):
-    while( have_posts() ): the_post(); ?>
-    <div>
+    while( have_posts() ): the_post();?>
+
+    <div id=<?php echo get_post_meta($post->ID, "id", true); ?> >
       <h1><?php the_title(); ?></h1>
       <p><?php the_content(); ?></p>
-      <p><?php  ?></p>
     </div>
+
     <?php
     endwhile;
   endif;
